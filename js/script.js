bbd659154666c9c502c93d7334afc30f32e8ef5e
Francescolapiana <Lushika1219@gmail.com>
2022-11-08 17:26:08 +0100

    Finished

--- js/script.js
+++ js/script.js
@@ -2,10 +2,19 @@ const {createApp} = Vue;
 
 const app = createApp({
     data(){
-       
+       return {
+        vals: []
+       }
     },
 
     methods: {
+        chiamataApi(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res)=>{
+                console.log(res.data.response);
+                this.vals.push(res.data.response);
+             })
+        },
+
 
     },
 
@@ -18,12 +27,14 @@ const app = createApp({
     },
 
     created(){
-     
+        for (let index = 0; index < 10; index++) {
+            this.chiamataApi();            
+        }
     },
 
     beforeCreate(){
-        
+
     },
 
 });
-app.mount('#foil')
+app.mount('#app')
